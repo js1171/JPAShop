e9303dc3ba6d117a5dd018a83781acc97f9401b6
js1171 <bhd1171@naver.com>
2024-01-01 17:32:52 +0900

    :sparkles: 회원 가입 기능 (POST) / 이름 validation 적용

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -1,9 +1,13 @@
 package jpabook.jpashop.controller;
 
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -19,4 +23,26 @@ public class MemberController {
         return "members/createMemberForm";
     }
 
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        // result에 error가 있으면 createMemberForm으로 넘겨줌
+        if(result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+        
+        // Address 만들어줌 (city, street, zipcode 조합)
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        // member 객체에 이름과 주소 세팅
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        // DB에 저장
+        memberService.join(member);
+
+        // 페이지로 리다이렉트
+        return "redirect:/";
+    }
 }
